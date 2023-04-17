d37819098592a3e199a3d5be9beabe38abf41338
anastan588 <anastasiya5.88@mail.ru>
2023-04-17 21:00:56 +0300

    init: add solution of dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,68 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  console.log(members);
+  console.log(typeof members);
+  if (Array.isArray(members) && members.length > 0) {
+    let resultArray = [];
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === "string") {
+        for (let m = 0; m < members[i].length; m++) {
+          if (members[i][m] !== " ") {
+            resultArray.push(members[i][m]);
+            break;
+          }
+        }
+      }
+    }
+    console.log(resultArray);
+    resultArray = resultArray.map((x) => x.toUpperCase());
+    console.log(resultArray);
+    resultArray = resultArray.sort();
+    console.log(resultArray);
+    let result = resultArray.join("");
+    console.log(result);
+    return result;
+  } else {
+    return false;
+  }
 }
 
+// createDreamTeam(["Matt", "Ann", "Dmitry", "Max"]);
+// createDreamTeam([]);
+// createDreamTeam(3);
+// createDreamTeam(3.312312);
+// createDreamTeam(false);
+// createDreamTeam(null);
+// createDreamTeam(undefined);
+// createDreamTeam({ 'foo': 'bar' });
+// createDreamTeam([
+//   ["David Abram"],
+//   ["Robin Attfield"],
+//   "Thomas Berry",
+//   ["Paul R.Ehrlich"],
+//   "donna Haraway",
+//   " BrIaN_gOodWiN  ",
+//   {
+//     0: "Serenella Iovino",
+//   },
+//   "Erazim Kohak",
+//   "  val_plumwood",
+// ]);
+
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
