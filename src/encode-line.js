cfecf7a74fb7f7923f691ebe7db818b64aeb9579
anastan588 <anastasiya5.88@mail.ru>
2023-04-17 21:01:39 +0300

    init: add solution of encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  console.log(str);
+  if (str.length === 0) {
+    return str;
+  } else {
+    let startArray = str.split("");
+    console.log(startArray);
+    let resultArray = [];
+    let count = 0;
+    let previousCount = 1;
+    for (let i = 0; i < startArray.length; i++) {
+      if (startArray[i] === startArray[i - 1]) {
+        count = count + 1;
+        previousCount = previousCount + 1;
+      } else {
+        count = 0;
+      }
+      console.log(i === startArray.length - 1);
+      console.log(previousCount);
+      if (count === 0 && previousCount > 1) {
+        if (i === startArray.length - 1) {
+          resultArray.push(previousCount);
+          resultArray.push(startArray[i - 1]);
+          resultArray.push(startArray[i]);
+        } else {
+          resultArray.push(previousCount);
+          resultArray.push(startArray[i - 1]);
+          previousCount = 1;
+        }
+      } else if (i === startArray.length - 1 && previousCount > 1) {
+        resultArray.push(previousCount);
+        resultArray.push(startArray[i]);
+      } else if (
+        count === 0 &&
+        i === 0 &&
+        startArray[i] !== startArray[i + 1]
+      ) {
+        console.log(startArray[i]);
+        resultArray.push(startArray[i]);
+      } else if (
+        count === 0 &&
+        i !== 0 &&
+        startArray[i] !== startArray[i + 1]
+      ) {
+        resultArray.push(startArray[i]);
+      }
+    }
+    console.log(resultArray);
+    let result = resultArray.join("");
+    console.log(result);
+    return result;
+  }
 }
 
+// encodeLine("aaaatttt");
+// encodeLine("aabbccc");
+// encodeLine("abbcca");
+// encodeLine("xyz");
+// encodeLine("");
+
 module.exports = {
-  encodeLine
+  encodeLine,
 };
