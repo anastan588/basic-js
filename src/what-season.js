4f917710fc36488affd4969cf3817f63a67c1b14
anastan588 <anastasiya5.88@mail.ru>
2023-04-17 20:59:37 +0300

    init: add solution of what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,20 +1,41 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require("../extensions/index.js");
 
-/**
- * Extract season from given date and expose the enemy scout!
- * 
- * @param {Date | FakeDate} date real or fake date
- * @returns {String} time of the year
- * 
- * @example
- * 
- * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
- */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// /**
+//  * Extract season from given date and expose the enemy scout!
+//  *
+//  * @param {Date | FakeDate} date real or fake date
+//  * @returns {String} time of the year
+//  *
+//  * @example
+//  *
+// // getSeason(new Date(2020, 02, 31)) => 'spring'
+//  *
+//  */
+function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  if (!(date instanceof Date) || Object.keys(date).length) throw new Error("Invalid date!");
+  let month = date.getMonth();
+  let season = "";
+  console.log(month);
+  if (month >= 0 && month <= 1) {
+    season = "winter";
+  } else if (month >= 2 && month <= 4) {
+    season = "spring";
+  } else if (month >= 5 && month <= 7) {
+    season = "summer";
+  } else if (month >= 8 && month <= 10) {
+    season = "autumn";
+  } else if (month === 11) {
+    season = "winter";
+  }
+  return season;
 }
+
+// let data = new Date(2020, 02, 31);
+// getSeason(data);
+// console.log(data);
 
 module.exports = {
   getSeason
